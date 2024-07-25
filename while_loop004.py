9a08e2def0dd055d9ff4b2e17e8769fbd125c3d5
Pasuuu-yahoo <pasuthorn1406@gmail.com>
2024-07-25 10:40:00 +0700

    Lecture04

--- while_loop004.py
+++ while_loop004.py
@@ -2,16 +2,17 @@ import random
 
 print("what is my magic number (1 to 100) ?")
 mynumber = random.randint(1,100)
+
 ntries   = 1
 yourguess = -1
-while ntries < 7 and yourguess != mynumber:
+while ntries < 11 and yourguess != mynumber:
     msg = str(ntries) + " >> "
-    if (ntries == 6) :
+    if (ntries == 10) :
         print("Your last chance")
     yourguess = int(input(msg))
     if (yourguess > mynumber):
         print("--> Too high")
-    else:
+    elif yourguess < mynumber:
         print("--> Too low")
     ntries += 1
 
