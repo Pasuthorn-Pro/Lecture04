0bbbed9fc5feea84aaad144354d952a952d55faf
Pasuuu-yahoo <pasuthorn1406@gmail.com>
2024-07-25 11:46:16 +0700

    Lecture04

---
+++ Exercise03(alt).py
@@ -0,0 +1,7 @@
+colum = int(input("Enter number of columns: "))
+for i in range(1, 101):
+
+    print(i, end=' ')
+
+    if i% colum == 0:
+        print()
--- Exercise03.py
+++ Exercise03.py
@@ -1,2 +1,20 @@
-for i in range(1,101,1):
-    print(i)
+colum = int(input("Enter number of columns: "))
+count = 0
+
+for i in range(1, 101):
+
+    print(i, end=' ')
+
+
+    count += 1
+
+
+    if count == colum:
+
+        print()
+       
+        count = 0
+
+
+if count != 0:
+    print()
