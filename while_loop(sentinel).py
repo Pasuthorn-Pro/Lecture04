91c06490f86ba3ad053debaca01967da52293533
Pasuuu-yahoo <pasuthorn1406@gmail.com>
2024-07-25 10:18:18 +0700

    Lecture04

--- while_loop(sentinel).py
+++ while_loop(sentinel).py
@@ -4,9 +4,8 @@ def print_asterisks(rows, cols):
             print('*', end=' ')
         print()
 
-# Taking input for rows and columns
+
 rows = int(input("Enter number of rows: "))
 cols = int(input("Enter number of columns: "))
 
-# Call the function to print asterisks
 print_asterisks(rows, cols)
